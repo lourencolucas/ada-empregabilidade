f2f08407bd1ed803da1e553865c4cb3988112242
Lucas Oliveira <49119918+lourencolucas@users.noreply.github.com>
2024-09-20 21:59:40 -0300

    Update main.py
    Feat: add addition, subtraction, multiplication and division functions

---
+++ main.py
@@ -0,0 +1,14 @@
+def adicao(x, y):
+    return x + y
+
+def subtrair(x, y):
+    return x - y
+
+def multiplicar(x, y):
+    return x * y
+
+def dividir(x, y):
+    if y != 0:
+        return x / y
+    else:
+        return "Erro! Divisão por zero."
